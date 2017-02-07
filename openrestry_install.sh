24870e5a4a9adbfd60771ccf7b0b77f5ab627f65
jinxiao <jinxiao2010@gmail.com>
2017-02-07 16:53:32 +0800

    add initd script

--- openrestry_install.sh
+++ openrestry_install.sh
@@ -7,3 +7,7 @@ tar -xvf openrestry-$openrestry_version -C /tmp
 cd /tmp/openrestry-$openrestry_version
 ./configure 
 make && make install
+wget https://gist.githubusercontent.com/vdel26/8805927/raw/249f907e465e98ac099437025218a15e55a34b4c/nginx -O /etc/init.d/nginx
+chkconfig --add nginx
+chkconfig nginx on
+service nginx start
